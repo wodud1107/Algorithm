62ae1b73fda5327b561087866659e4da50126084
wodud1107 <wodud7992@naver.com>
2025-01-23 10:59:14 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,8 +1,6 @@
 import java.io.*;
 
 public class Main {
-    static int cnt1;
-    static int cnt2;
     static int[] fib;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -10,25 +8,15 @@ public class Main {
         fib=new int[N+1];
 
         StringBuilder sb=new StringBuilder();
-        fib1(N);
-        fib2(N);
-        sb.append(cnt1).append(' ').append(cnt2);
+        sb.append(fibonacci(N)).append(' ').append(N-2);
         System.out.println(sb);
 
         br.close();
     }
-    static int fib1(int n){
-        if (n<=2){
-            cnt1++;
-            return 1;
-        }
-        else return fib1(n-1)+fib1(n-2);
-    }
-    static int fib2(int n){
+    static int fibonacci(int n){
         fib[1]=1;
         fib[2]=1;
         for (int i=3; i<=n; i++){
-            cnt2++;
             fib[i]=fib[i-1]+fib[i-2];
         }
         return fib[n];
