81a864ab5d2de604bfad29a18531d8b60a433ae7
wodud1107 <wodud7992@naver.com>
2025-01-27 16:44:23 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11468 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,10 +3,9 @@ import java.io.*;
 public class Main {
     static int[] step;
     static int[] dp;
-    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
+        int N=Integer.parseInt(br.readLine());
         step=new int[N+1];
         dp=new int[N+1];
 
@@ -14,12 +13,12 @@ public class Main {
             step[i]=Integer.parseInt(br.readLine());
         }
 
-        System.out.println(dp(N));
+        dp(N);
+        System.out.println(dp[N]);
 
         br.close();
     }
-    static int dp(int N){
-        dp[0]=0;
+    static void dp(int N){
         dp[1]=step[1];
 
         if (N>=2){
@@ -29,7 +28,5 @@ public class Main {
         for (int i=3; i<=N; i++){
             dp[i]=Math.max(dp[i-2], dp[i-3]+step[i-1])+step[i];
         }
-
-        return dp[N];
     }
 }
