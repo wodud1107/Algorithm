7a1ca1179710b8bd3ea9210ae3d0cb6d0c62843a
JaeYoung Kim <wodud7992@naver.com>
2025-03-19 15:32:15 +0900

    [level 1] Title: 모의고사, Time: 1.23 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] answers){
+        int[] first={1,2,3,4,5};
+        int[] second={2,1,2,3,2,4,2,5};
+        int[] third={3,3,1,1,2,2,4,4,5,5};
+
+        int[] result=new int[3];
+    
+        result[0]=comp(first, answers);
+        result[1]=comp(second, answers);
+        result[2]=comp(third, answers);
+
+        int max=Integer.MIN_VALUE;
+        for (int i=0; i<result.length; i++){
+            if (max<result[i]) max=result[i];
+        }
+
+        ArrayList<Integer> ans=new ArrayList<>();
+        for (int i=0; i<result.length; i++){
+            if (max==result[i]){
+                ans.add(i+1);
+            }
+        }
+
+        return ans;
+    }
+    static int comp(int[] arr, int[] answers){
+        int result=0;
+        for (int i=0; i<answers.length; i++){
+            int j=i%arr.length;
+            if (answers[i]==arr[j]) result++;
+        }
+        return result;
+    }
+}
