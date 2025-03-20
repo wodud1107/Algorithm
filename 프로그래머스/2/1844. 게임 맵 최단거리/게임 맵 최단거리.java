28d3ae00ab9cf545d91f7cd3ffeb6742f13dea0e
JaeYoung Kim <wodud7992@naver.com>
2025-03-20 18:28:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.86 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,42 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx=new int[]{-1, 1, 0, 0};
-    static int[] dy=new int[]{0, 0, -1, 1};
+    static int[] dx={-1, 1, 0, 0};
+    static int[] dy={0, 0, -1, 1};
     public int solution(int[][] maps) {
-        int answer = 0;
         int n=maps.length;
         int m=maps[0].length;
         
-        int[][] visited=new int[n][m];
-        
-        bfs(maps, visited);
-        answer=visited[n-1][m-1];
-        
-        if (answer==0) answer=-1;
-        return answer;
+        return bfs(maps, n, m);
     }
-    static void bfs(int[][] maps, int[][] visited){
+    static int bfs(int[][] maps, int n, int m){
         Queue<int[]> queue=new LinkedList<>();
-        queue.add(new int[]{0,0});
-        visited[0][0]=1;
         
-        while(!queue.isEmpty()){
+        queue.add(new int[]{0, 0, 1});
+        
+        while (!queue.isEmpty()){
             int[] current=queue.poll();
             int x=current[0];
             int y=current[1];
+            int distance=current[2];
+            
+            if (x == n-1 && y == m-1){
+                return distance;
+            }
             
             for (int i=0; i<4; i++){
                 int nx=x+dx[i];
                 int ny=y+dy[i];
                 
-                if (nx < 0 || nx > maps.length-1 || ny < 0 || ny > maps[0].length-1){
-                    continue;
-                }
-                
-                if (visited[nx][ny] == 0 && maps[nx][ny]==1){
-                    visited[nx][ny]=visited[x][y]+1;
-                    queue.add(new int[]{nx, ny});
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m){
+                    if (maps[nx][ny]==1){
+                        maps[nx][ny]=0;
+                        queue.add(new int[]{nx, ny, distance+1});
+                    }
                 }
             }
         }
+        
+        return -1;
     }
 }
