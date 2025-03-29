2404bc81887d0c1cdf96593ca3e61502d8480458
JaeYoung Kim <wodud7992@naver.com>
2025-03-29 23:47:17 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int A, B;
+    std::cin >> A >> B;
+    if (A > B){
+        std::cout << ">" << std::endl;
+    } else if (A < B) {
+        std::cout << "<" << std::endl;
+    } else {
+        std::cout << "==" << std::endl;
+    }
+    return 0;
+}
