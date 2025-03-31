d988ceaff1dbdef5e556dc0454795bddedcb3cc3
JaeYoung Kim <wodud7992@naver.com>
2025-03-31 14:34:18 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+    
+    for (int i=0; i<N; i++){
+        for (int j=0; j<=i; j++){
+            std::cout << '*';
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
