9bca4c4ce44353aa471b5035eb329a1d3cdb27aa
JaeYoung Kim <wodud7992@naver.com>
2025-04-02 15:22:18 +0900

    [Bronze II] Title: 수 정렬하기, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+
+    int arr[N];
+    for (int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+N);
+    for (int i = 0; i < N; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
