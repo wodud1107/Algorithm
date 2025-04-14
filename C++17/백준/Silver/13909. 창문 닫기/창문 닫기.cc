0f0969e3e28e9dbb2c796656e9ea87e122684b16
JaeYoung Kim <wodud7992@naver.com>
2025-04-14 18:57:34 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int answer = sqrt(N);
+
+    cout << answer << '\n';
+
+    return 0;
+}
