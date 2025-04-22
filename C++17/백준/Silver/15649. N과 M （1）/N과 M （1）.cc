83ab59ef3a1f827b6d91127c566b279e2e4e7145
JaeYoung Kim <wodud7992@naver.com>
2025-04-22 15:35:07 +0900

    [Silver III] Title: N과 M (1), Time: 8 ms, Memory: 3588 KB -BaekjoonHub

--- C++17/백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ C++17/백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -4,21 +4,28 @@ using namespace std;
 int N, M;
 bool visited[9];
 int num[9];
+string result;
 
 void dfs(int depth);
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     cin >> N >> M;
+
     dfs(0);
+    cout << result;
+    
     return 0;
 }
 
 void dfs(int depth){
     if (depth == M){
         for (int i = 0; i < M; i++){
-            cout << num[i] << ' ';
+            result += to_string(num[i]) + ' ';
         }
-        cout << '\n';
+        result += '\n';
         return;
     }
 
