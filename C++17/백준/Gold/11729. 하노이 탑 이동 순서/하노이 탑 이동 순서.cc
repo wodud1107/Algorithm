a95bf69c6805a9c64557c8ae430c0f8d68c33b72
JaeYoung Kim <wodud7992@naver.com>
2025-04-22 14:40:40 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 52 ms, Memory: 12764 KB -BaekjoonHub

--- C++17/백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ C++17/백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,9 +1,12 @@
 #include <cstdio>
+#include <string>
+
+std::string result;
 
 void hanoi(int N, int a, int b, int c){
     if (N >= 1) {
         hanoi(N - 1, a, c, b);
-        printf("%d %d\n", a, c);
+        result += std::to_string(a) + ' ' + std::to_string(c) + '\n';
         hanoi(N - 1, b, a, c);
         return;
     }
@@ -15,6 +18,7 @@ int main(){
     
     printf("%d\n", (1 << N) - 1);
     hanoi(N, 1, 2, 3);
+    printf("%s", result.c_str());
 
     return 0;
 }
