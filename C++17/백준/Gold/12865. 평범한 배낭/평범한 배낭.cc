4ada7c23aae6dd1c05a878bb4df5cc97cf238cec
JaeYoung Kim <wodud7992@naver.com>
2025-05-06 23:37:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2284 KB -BaekjoonHub

--- C++17/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ C++17/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstring>
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -9,21 +9,22 @@ int main(){
     int N, K;
     cin >> N >> K;
 
-    int W[N], V[N];
-    for (int i = 0; i < N; i++){
-        cin >> W[i] >> V[i];
+    int arr[N + 1][2];
+    for (int i = 1; i <= N; i++){
+        cin >> arr[i][0] >> arr[i][1];
     }
 
-    int dp[N + 1][K + 1];
-    memset(dp, 0, sizeof(dp));
+    int dp[K + 1];
+    fill(dp, dp + K + 1, 0);
     for (int i = 1; i <= N; i++){
-        for (int j = 1; j <= K; j++){
-            if (W[i - 1] <= j) dp[i][j] = max(dp[i - 1][j], V[i - 1] + dp[i - 1][j - W[i - 1]]);
-            else dp[i][j] = dp[i - 1][j];
+        int W = arr[i][0];
+        int V = arr[i][1];
+        for (int j = K; j >= W; j--){
+            dp[j] = max(dp[j], dp[j - W] + V);
         }
     }
 
-    cout << dp[N][K] << '\n';
+    cout << dp[K] << '\n';
 
     return 0;
 }
