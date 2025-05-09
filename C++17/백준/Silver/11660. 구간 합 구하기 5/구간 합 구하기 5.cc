811a7991d18849583702ca6756e5ca9b2305f5dc
JaeYoung Kim <wodud7992@naver.com>
2025-05-09 23:22:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 6004 KB -BaekjoonHub

--- C++17/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ C++17/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -9,14 +9,13 @@ int main(){
     int N, M;
     cin >> N >> M;
 
-    int num[N][N + 1];
+    int num[N + 1][N + 1];
     memset(num, 0, sizeof(num));
-    for (int i = 0; i < N; i++){
-        num[i][0] = 0;
+    for (int i = 1; i <= N; i++){
         for (int j = 1; j <= N; j++){
             int x;
             cin >> x;
-            num[i][j] = num[i][j - 1] + x;
+            num[i][j] = num[i - 1][j] + num[i][j - 1] - num[i -1][j - 1] + x;
         }
     }
 
@@ -24,12 +23,7 @@ int main(){
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
-        int sum = 0;
-        for (int i = x1 - 1; i < x2; i++){
-            sum += num[i][y2] - num[i][y1 - 1];
-        }
-
-        cout << sum << '\n';
+        cout << num[x2][y2] - num[x1 - 1][y2] - num[x2][y1 - 1] + num[x1 - 1][y1 - 1] << '\n';
     }
 
     return 0;
