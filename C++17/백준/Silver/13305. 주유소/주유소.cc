9e63cc1fb6cb2835e3358a38e7f2bb4cc80348fe
JaeYoung Kim <wodud7992@naver.com>
2025-05-20 12:37:40 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 3460 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Silver/13305. 주유소/주유소.cc
+++ C++17/백준/Silver/13305. 주유소/주유소.cc
@@ -9,28 +9,19 @@ int main(){
     cin >> N;
 
     long long dist[100000], price[100001];
-    long long total_d = 0;
-    for (int i = 0; i < N - 1; i++){
-        int x;
-        cin >> x;
-        dist[i] = x;
-
-        total_d += x;
-    }
-
+    for (int i = 0; i < N - 1; i++) cin >> dist[i];
     for (int i = 0; i < N; i++) cin >> price[i];
 
     long long answer = 0;
-    long long min_price = price[0] * total_d;
-    long long cumulative_price = 0;
+    long long min_price = price[0];
     for (int i = 0; i < N - 1; i++){
-        long long rest_d = total_d - dist[i];
-        
-        cumulative_price += dist[i] * price[i];
-        min_price = min(min_price, price[i + 1] * rest_d + cumulative_price);
+        if (min_price > price[i]){
+            min_price = price[i];
+        }
+        answer += min_price * dist[i];
     }
 
-    cout << min_price << '\n';
+    cout << answer << '\n';
 
     return 0;
 }
