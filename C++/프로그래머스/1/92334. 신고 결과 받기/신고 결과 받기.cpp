0894c0404b75a588541e48229ced4d7cbe616f29
JaeYoung Kim <wodud7992@naver.com>
2025-10-10 22:06:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 329.49 ms, Memory: 40.1 MB -BaekjoonHub

--- C++/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ C++/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -8,24 +8,25 @@ using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer(id_list.size(), 0);
+    
     map<string, int> id_to_idx;
     for (int i = 0; i < id_list.size(); i++) {
         id_to_idx[id_list[i]] = i;
     }
     
     map<string, set<string>> reported_ids;
-    for (const string& rep : report) {
-        stringstream ss(rep);
+    for (const auto& repo : report) {
+        stringstream ss(repo);
         string reporter, reported;
         ss >> reporter >> reported;
         
         reported_ids[reported].insert(reporter);
     }
     
-    for (auto const& [reported, reporters] : reported_ids) {
+    for (const auto& [reported, reporters] : reported_ids) {
         if (reporters.size() >= k) {
-            for (const string& reporter_id : reporters) {
-                answer[id_to_idx[reporter_id]]++;
+            for (const string& reporter : reporters) {
+                answer[id_to_idx[reporter]]++;
             }
         }
     }
