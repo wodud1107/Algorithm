2e0a2c1f948045a4d58d7bca753c8394377ccfac
JaeYoung Kim <wodud7992@naver.com>
2025-12-31 15:51:25 +0900

    [Gold V] Title: 숫자고르기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+ls = [0]
+for _ in range(1, N + 1):
+    ls.append(int(input()))
+
+ans_list = []
+def dfs(start, current, visited):
+    visited[current] = True
+    next = ls[current]
+    
+    if not visited[next]:
+        dfs(start, next, visited)
+    elif visited[next] and next == start:
+        ans_list.append(start)
+
+for i in range(1, N + 1):
+    visited = [False] * (N + 1)
+    dfs(i, i, visited)
+
+print(len(ans_list))
+print(*ans_list, sep = '\n')
