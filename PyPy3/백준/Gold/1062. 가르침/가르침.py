3a8c09c16ada85f0ca0cce06f3cf17a5efb8582c
JaeYoung Kim <wodud7992@naver.com>
2026-03-03 16:32:13 +0900

    [Gold IV] Title: 가르침, Time: 192 ms, Memory: 110736 KB -BaekjoonHub

--- PyPy3/백준/Gold/1062. 가르침/가르침.py
+++ PyPy3/백준/Gold/1062. 가르침/가르침.py
@@ -9,25 +9,31 @@ words = [input().rstrip() for _ in range(N)]
 if K < 5: print(0); exit(0)
 elif K == 26: print(N); exit(0)
 
-learned = set(['a', 'n', 't', 'i', 'c'])
-remaining = [chr(i) for i in range(ord('a'), ord('z') + 1) if chr(i) not in learned]
+learned = 0
+for char in 'antic':
+    learned |= (1 << (ord(char) - ord('a')))
 
-word_sets = []
+word_masks = []
 for word in words:
-    word_sets.append(set(word[4:-4]))
-    
+    mask = 0
+    for char in word:
+        mask |= (1 << (ord(char) - ord('a')))
+    word_masks.append(mask)
+
+remaining = [chr(i) for i in range(ord('a'), ord('z') + 1) if chr(i) not in 'antic']
+
 max_cnt = 0
 for combs in combinations(remaining, K - 5):
-    current_learned = learned.copy()
-    
+    current_learned = learned
     for char in combs:
-        current_learned.add(char)
-        
+        current_learned |= (1 << (ord(char) - ord('a')))
+    
     count = 0
-    for w_set in word_sets:
-        if w_set.issubset(current_learned):
+    for word_mask in word_masks:
+        if (word_mask & current_learned) == word_mask:
             count += 1
-    
-    if count > max_cnt: max_cnt = count
-    
+            
+    if count > max_cnt:
+        max_cnt = count
+
 print(max_cnt)
