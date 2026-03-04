898cc6883f7d98792520d6c837c5f5f7c04e505d
JaeYoung Kim <wodud7992@naver.com>
2026-03-04 17:48:20 +0900

    [Gold I] Title: 계단 수, Time: 112 ms, Memory: 116556 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/1562. 계단 수/계단 수.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+MOD = 1_000_000_000
+N = int(input())
+
+dp = [[[0] * 1024 for _ in range(10)] for _ in range(N + 1)]
+
+if N >= 1:
+    for j in range(1, 10):
+        dp[1][j][1 << j] = 1
+
+for i in range(2, N + 1):
+    for j in range(10):
+        for mask in range(1024):
+            if dp[i - 1][j][mask] == 0:
+                continue
+            
+            if j > 0:
+                next_mask = mask | (1 << (j - 1))
+                dp[i][j - 1][next_mask] = (dp[i][j - 1][next_mask] + dp[i - 1][j][mask]) % MOD
+                
+            if j < 9:
+                next_mask = mask | (1 << (j + 1))
+                dp[i][j + 1][next_mask] = (dp[i][j + 1][next_mask] + dp[i - 1][j][mask]) % MOD
+                
+if N >= 1:
+    ans = 0
+    for j in range(10):
+        ans = (ans + dp[N][j][1023]) % MOD
+    print(ans)
+else: print(0)
