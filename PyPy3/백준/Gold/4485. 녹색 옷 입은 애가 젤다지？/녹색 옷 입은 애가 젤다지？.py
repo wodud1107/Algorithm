7478359682673b5662ce98b82c756cc5290baacb
JaeYoung Kim <wodud7992@naver.com>
2026-03-18 16:48:05 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 160 ms, Memory: 111900 KB -BaekjoonHub

--- PyPy3/백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ PyPy3/백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,22 +1,27 @@
 import sys
-from collections import deque
+import heapq
 input = sys.stdin.readline
 
 def bfs(N, board):
-    visited = [[float('inf')] * N for _ in range(N)]
-    visited[0][0] = board[0][0]
-    q = deque([(0, 0)])
+    dist = [[float('inf')] * N for _ in range(N)]
+    dist[0][0] = board[0][0]
+    q = [(board[0][0], 0, 0)]
     
     while q:
-        x, y = q.popleft()
-            
+        cost, x, y = heapq.heappop(q)
+        
+        if x == N - 1 and y == N - 1:
+            return cost
+        
+        if cost > dist[x][y]:
+            continue
+        
         for (dx, dy) in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             nx, ny = x + dx, y + dy
             
-            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] > visited[x][y] + board[nx][ny]:
-                visited[nx][ny] = visited[x][y] + board[nx][ny]
-                q.append((nx, ny))
-    return visited[-1][-1]
+            if 0 <= nx < N and 0 <= ny < N and dist[nx][ny] > cost + board[nx][ny]:
+                dist[nx][ny] = cost + board[nx][ny]
+                heapq.heappush(q, (dist[nx][ny], nx, ny))
 
 problem = 1
 while True:
