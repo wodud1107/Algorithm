042203a3aa8d182cf0208f141c3b5fab7b68a5ab
JaeYoung Kim <wodud7992@naver.com>
2026-03-24 13:57:03 +0900

    [Gold III] Title: Ax+Bsin(x)=C ②, Time: 108 ms, Memory: 108384 KB -BaekjoonHub

--- PyPy3/백준/Gold/14786. Ax＋Bsin（x）＝C ②/Ax＋Bsin（x）＝C ②.py
+++ PyPy3/백준/Gold/14786. Ax＋Bsin（x）＝C ②/Ax＋Bsin（x）＝C ②.py
@@ -2,18 +2,17 @@ import sys
 import math
 input = sys.stdin.readline
 
-
 A, B, C = map(int, input().split())
 
 s, e = 0, 100_000
 
-while s <= e:
+while e - s > 1e-9:
     mid = (s + e) / 2
     
     if (A * mid + B * math.sin(mid)) ** 2 > C ** 2:
-        e = mid - 10 ** (-9)
+        e = mid
     elif (A * mid + B * math.sin(mid)) ** 2 < C ** 2:
-        s = mid + 10 ** (-9)
+        s = mid
     else:
         break
     
