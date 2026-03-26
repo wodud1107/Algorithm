bf71256da0ee23b050057d9f97c1857aa81a2f82
JaeYoung Kim <wodud7992@naver.com>
2026-03-26 22:06:23 +0900

    [Gold III] Title: 게임 닉네임, Time: 176 ms, Memory: 28828 KB -BaekjoonHub

---
+++ C++17/백준/Gold/16934. 게임 닉네임/게임 닉네임.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+int N;
+string s;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+    unordered_set<string> prefixes;
+    unordered_map<string, int> word_cnt;
+
+    while (N--) {
+        cin >> s;
+
+        word_cnt[s]++;
+
+        string alias = "";
+        bool found_alias = false;
+
+        for (int i = 1; i <= s.size(); i++) {
+            string prefix = s.substr(0, i);
+            auto it = prefixes.find(prefix);
+
+            if (!found_alias && it == prefixes.end()) {
+                alias = prefix;
+                found_alias = true;
+            }
+            
+            prefixes.insert(prefix);
+        }
+
+        if (!found_alias) {
+            alias = s;
+            if (word_cnt[s] > 1) alias += to_string(word_cnt[s]);
+        }
+
+        cout << alias << '\n';
+    } 
+
+    return 0;
+}
