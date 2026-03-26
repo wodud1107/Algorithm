6554f95c75e3de40510baf489ecc0dd5047e5278
JaeYoung Kim <wodud7992@naver.com>
2026-03-26 13:26:12 +0900

    [Gold IV] Title: LCS 3, Time: 164 ms, Memory: 120412 KB -BaekjoonHub

--- PyPy3/백준/Gold/1958. LCS 3/LCS 3.py
+++ PyPy3/백준/Gold/1958. LCS 3/LCS 3.py
@@ -1,21 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-s = [input().strip() for _ in range(3)]
-s.sort(key=len)
-longest = s[2]
+s1 = input().strip()
+s2 = input().strip()
+s3 = input().strip()
 dp = [[[0] * 101 for _ in range(101)] for _ in range(101)]
 
-for i in range(1, len(s[0]) + 1):
-    for j in range(1, len(s[1]) + 1):
-        for k in range(1, len(s[2]) + 1):
-            shortest = s[0][i - 1]
-            middle = s[1][j - 1]
-            longest = s[2][k - 1]
+for i in range(1, len(s1) + 1):
+    for j in range(1, len(s2) + 1):
+        for k in range(1, len(s3) + 1):
+            c1 = s1[i - 1]
+            c2 = s2[j - 1]
+            c3 = s3[k - 1]
             
-            if shortest == middle == longest:
+            if c1 == c2 == c3:
                 dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1
             else:
                 dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1])
 
-print(dp[len(s[0])][len(s[1])][len(s[2])])
+print(dp[len(s1)][len(s2)][len(s3)])
