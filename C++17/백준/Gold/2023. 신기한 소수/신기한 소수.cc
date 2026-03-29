76846e2b076aba1be95175ba3c9193bdd1c2f269
JaeYoung Kim <wodud7992@naver.com>
2026-03-29 17:43:01 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N;
+
+bool is_prime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+
+    return true;
+}
+
+void dfs(int num, int size) {
+    if (size == N) {
+        if (is_prime(num)) cout << num << '\n';
+        return;
+    }
+
+    int odd_2[6] = { 1, 2, 3, 5, 7, 9 };
+
+    for (int o : odd_2) {
+        int next = num * 10 + o;
+        if (!is_prime(next)) continue;
+        dfs(next, size + 1);
+    }
+
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+    dfs(0, 0);
+
+    return 0;
+}
