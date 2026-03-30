cd4d3e82b8c983202b392b76d73d0f6faa882846
JaeYoung Kim <wodud7992@naver.com>
2026-03-30 15:20:33 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Gold/14502. 연구소/연구소.cc
+++ C++17/백준/Gold/14502. 연구소/연구소.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 using pii = pair<int, int>;
 
@@ -11,14 +10,13 @@ vector<pii> viruses;
 int answer = 0;
 
 void spread(int curr_board[8][8]) {
-    queue<pii> q(deque<pii>(viruses.begin(), viruses.end()));
-
     int dx[4] = {-1, 1, 0, 0};
     int dy[4] = {0, 0, -1, 1};
 
-    while (!q.empty()) {
-        auto [x, y] = q.front();
-        q.pop();
+    vector<pii> q = viruses;
+    int front = 0;
+    while (front < (int)q.size()) {
+        auto [x, y] = q[front++];
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
@@ -26,7 +24,7 @@ void spread(int curr_board[8][8]) {
 
             if (0 <= nx && nx < N && 0 <= ny && ny < M && curr_board[nx][ny] == 0) {
                 curr_board[nx][ny] = 2;
-                q.emplace(nx, ny);
+                q.emplace_back(nx, ny);
             }
         }
     }
@@ -46,7 +44,8 @@ void dfs(int cnt, int start) {
         int cnt = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (spread_board[i][j] == 0) cnt++;
+                if (spread_board[i][j]) continue;
+                else cnt++;
             }
         }
         answer = answer > cnt ? answer : cnt;
