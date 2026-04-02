e4384d2e48bb3abd61114a46db1a6c6280e90234
JaeYoung Kim <wodud7992@naver.com>
2026-04-02 13:40:28 +0900

    [Gold V] Title: 숫자고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2668. 숫자고르기/숫자고르기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+int nums[101];
+vector<int> selected;
+
+void dfs(int num, int start, vector<bool>& visited) {
+    visited[num] = true;
+    int next = nums[num];
+
+    if (!visited[next]) {
+        dfs(next, start, visited);
+    } else if (next == start) {
+        selected.push_back(start);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 1; i <= N; i++) {
+        vector<bool> visited(N, false);
+        dfs(i, i, visited);
+    }
+
+    cout << selected.size() << '\n';
+    for (int i = 0; i < selected.size(); i++) {
+        cout << selected[i] << '\n';
+    }
+
+    return 0;
+}
