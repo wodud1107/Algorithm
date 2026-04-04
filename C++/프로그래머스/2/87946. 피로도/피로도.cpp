6541fd14bdcb3457b4394491901b1861aa2cc9ab
JaeYoung Kim <wodud7992@naver.com>
2026-04-04 20:23:34 +0900

    [level 2] Title: 피로도, Time: 3.65 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/87946. 피로도/피로도.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int global_k;
+int answer = 0;
+
+void dfs(int cnt, vector<vector<int>>& dungeons, vector<bool> visited) {
+    if (answer < cnt) answer = cnt;
+    
+    for (int i = 0; i < dungeons.size(); i++) {
+        if (visited[i]) continue;
+        
+        if (global_k >= dungeons[i][0]) {
+            global_k -= dungeons[i][1];
+            visited[i] = true;
+            dfs(cnt + 1, dungeons, visited);
+            global_k += dungeons[i][1];
+            visited[i] = false;
+        }
+    }
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    global_k = k;
+    vector<bool> visited(dungeons.size(), false);
+    dfs(0, dungeons, visited);
+    return answer;
+}
