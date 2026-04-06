d39aa3e75d84f8e0d5bb22fbdca08e4bd941c26c
JaeYoung Kim <wodud7992@naver.com>
2026-04-06 14:28:00 +0900

    [Gold I] Title: 달빛 여우, Time: 616 ms, Memory: 166020 KB -BaekjoonHub

--- PyPy3/백준/Gold/16118. 달빛 여우/달빛 여우.py
+++ PyPy3/백준/Gold/16118. 달빛 여우/달빛 여우.py
@@ -5,11 +5,19 @@ INF = 1e15
 
 N, M = map(int, input().split())
 
-graph = [[] for _ in range(N + 1)]
+fox_graph = [[] for _ in range(N + 1)]
+wolf_graph = [[] for _ in range(2 * N + 1)]
 for i in range(M):
     a, b, d = map(int, input().split())
-    graph[a].append((b, 2 * d))
-    graph[b].append((a, 2 * d))
+    d *= 2
+    fox_graph[a].append((b, d))
+    fox_graph[b].append((a, d))
+
+    wolf_graph[a].append((b + N, d // 2))
+    wolf_graph[b].append((a + N, d // 2))
+    wolf_graph[a + N].append((b, 2 * d))
+    wolf_graph[b + N].append((a, 2 * d))
+    
     
 fox_time = [INF] * (N + 1)
 fox_time[1] = 0
@@ -19,33 +27,28 @@ while fox_q:
     
     if (fox_time[curr] < distance): continue
     
-    for (next, dist) in graph[curr]:
+    for (next, dist) in fox_graph[curr]:
         next_dist = distance + dist
         if (next_dist < fox_time[next]):
             fox_time[next] = next_dist
             heapq.heappush(fox_q, (next_dist, next))
             
-wolf_time = [[INF] * (N + 1) for _ in range(2)]
-wolf_time[0][1] = 0
-wolf_q = [(0, 1, 0)]
+wolf_time = [INF] * (2 * N + 1)
+wolf_time[1] = 0
+wolf_q = [(0, 1)]
 while wolf_q:
-    distance, curr, parity = heapq.heappop(wolf_q)
+    distance, curr = heapq.heappop(wolf_q)
     
-    if (wolf_time[parity][curr] < distance): continue
+    if (wolf_time[curr] < distance): continue
     
-    for (next, dist) in graph[curr]:
-        if (parity == 0):
-            next_dist = distance + dist // 2
-        else:
-            next_dist = distance + 2 * dist
-        next_parity = 1 - parity
-        if (next_dist < wolf_time[next_parity][next]):
-            wolf_time[next_parity][next] = next_dist
-            heapq.heappush(wolf_q, (next_dist, next, next_parity))
+    for (next, dist) in wolf_graph[curr]:
+        next_dist = distance + dist
+        if (next_dist < wolf_time[next]):
+            wolf_time[next] = next_dist
+            heapq.heappush(wolf_q, (next_dist, next))
             
 answer = 0
 for i in range(1, N + 1):
-    best_wolf_time = min(wolf_time[0][i], wolf_time[1][i])
-    if (fox_time[i] < best_wolf_time): answer += 1
+    if (fox_time[i] < min(wolf_time[i], wolf_time[i + N])): answer += 1
     
 print(answer)
