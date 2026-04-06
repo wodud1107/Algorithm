7f641abdf37e05fbf056ad810cf2dc22ec806ee4
JaeYoung Kim <wodud7992@naver.com>
2026-04-06 14:05:29 +0900

    [Gold I] Title: 달빛 여우, Time: 928 ms, Memory: 162184 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/16118. 달빛 여우/달빛 여우.py
@@ -0,0 +1,51 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+for i in range(M):
+    a, b, d = map(int, input().split())
+    graph[a].append((b, d))
+    graph[b].append((a, d))
+    
+fox_time = [float('inf')] * (N + 1)
+fox_time[1] = 0
+fox_q = [(0, 1)]
+while fox_q:
+    distance, curr = heapq.heappop(fox_q)
+    
+    if (fox_time[curr] < distance): continue
+    
+    for (next, dist) in graph[curr]:
+        next_dist = distance + dist
+        if (next_dist < fox_time[next]):
+            fox_time[next] = next_dist
+            heapq.heappush(fox_q, (next_dist, next))
+            
+wolf_time = [[float('inf')] * (N + 1) for _ in range(2)]
+wolf_time[0][1] = 0
+wolf_q = [(0, 1, 0)]
+while wolf_q:
+    distance, curr, parity = heapq.heappop(wolf_q)
+    
+    if (wolf_time[parity][curr] < distance): continue
+    
+    for (next, dist) in graph[curr]:
+        if (parity == 0):
+            next_dist = distance + dist / 2
+            next_parity = 1
+        else:
+            next_dist = distance + 2 * dist
+            next_parity = 0
+        if (next_dist < wolf_time[next_parity][next]):
+            wolf_time[next_parity][next] = next_dist
+            heapq.heappush(wolf_q, (next_dist, next, next_parity))
+            
+answer = 0
+for i in range(1, N + 1):
+    best_wolf_time = min(wolf_time[0][i], wolf_time[1][i])
+    if (fox_time[i] < best_wolf_time): answer += 1
+    
+print(answer)
