dfd39fed7004dd16990c757a891a1bc1a0e7466a
JaeYoung Kim <wodud7992@naver.com>
2026-04-11 20:15:57 +0900

    [Gold III] Title: 사다리 조작, Time: 1692 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ C++17/백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -2,14 +2,15 @@
 using namespace std;
 
 int N, M, H;
-int ladders[31][11];
+bool ladders[31][11];
 int answer = -1;
 
 bool check() {
     for (int j = 1; j <= N; j++) {
         int curr = j;
         for (int i = 1; i <= H; i++) {
-            curr = ladders[i][curr];
+            if (ladders[i][curr]) curr++;
+            else if (curr > 1 && ladders[i][curr - 1]) curr--;
         }
         if (curr != j) return false;
     }
@@ -30,15 +31,13 @@ void dfs(int x, int y, int cnt, int target) {
         int start = (i == x) ? y : 1;
 
         for (int j = start; j < N; j++) {
-            if (ladders[i][j] == j && ladders[i][j + 1] == j + 1) {
-                ladders[i][j] = j + 1;
-                ladders[i][j + 1] = j;
+            if (ladders[i][j]) continue;
+            if (j > 1 && ladders[i][j - 1]) continue;
+            if (j < N - 1 && ladders[i][j + 1]) continue;
 
-                dfs(i, j + 2, cnt + 1, target);
-
-                ladders[i][j] = j;
-                ladders[i][j + 1] = j + 1;
-            }
+            ladders[i][j] = true;
+            dfs(x, y + 1, cnt + 1, target);
+            ladders[i][j] = false;
         }
     }
 }
@@ -49,16 +48,10 @@ int main() {
 
     cin >> N >> M >> H;
 
-    for (int i = 1; i <= H; i++) {
-        for (int j = 1; j <= N; j++) {
-            ladders[i][j] = j;
-        }
-    }
-
     for (int i = 0; i < M; i++) {
         int a, b;
         cin >> a >> b;
-        swap(ladders[a][b], ladders[a][b + 1]);
+        ladders[a][b] = true;
     }
 
     if (check()) {
